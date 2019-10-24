972da8a1a99bc55862f05943cd8481cbe5c35ffa
GdoS100 <seisama100@yahoo.co.jp>
2019-10-24 15:27:05 +0900

    ATM1完成

--- 3-Message/client.c
+++ 3-Message/client.c
@@ -3,15 +3,16 @@
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <stdlib.h>
-#include <unistd/h>
+#include <unistd.h>
 
 #define BUF_SIZE 256
 #define MONEY_DIGIT_SIZE 10
 
 void DieWithError(char *);
 int prepare_client_socket(char *, int);
-void my_scanf(char *, _int);
+void my_scanf(char *, int);
 void commun(int);
+void read_until_delim(int, char *, char, int);
 
 int main(int argc, char *argv[])
 {
@@ -42,5 +43,72 @@ int prepare_client_socket(char *ipaddr, int port)
     struct sockaddr_in target;
     target.sin_family = AF_INET;
     target.sin_addr.s_addr = inet_addr(ipaddr);
-    target.sin_port
+    target.sin_port = htons(port);
+    if (connect(sock, (struct sockaddr *)&target, sizeof(target)) < 0)
+        DieWithError("connect() failed");
+
+    return sock;
+}
+void my_scanf(char *buf, int num_letter)
+{
+    char format[20];
+    sprintf(format, "%s%d%s", " ", num_letter, "s *[^\n]]");
+    scanf(format, buf);
+    getchar();
+}
+void commun(int sock)
+{
+    char msg[BUF_SIZE];
+    char cmd[2] = ""; //コマンド入力用
+    char withdraw[MONEY_DIGIT_SIZE + 1];
+    char deposit[MONEY_DIGIT_SIZE + 1];
+    printf("0:引き出し 1:預け入れ 2:残高照会\n");
+    printf("何をしますか? >");
+
+    my_scanf(cmd, 1);
+
+    switch (cmd[0])
+    {
+    case '0':
+        printf("引き出す金額を入力してください。>");
+        my_scanf(withdraw, MONEY_DIGIT_SIZE);
+        break;
+    case '1':
+        printf("預け入れる金額を入力してください>");
+        my_scanf(deposit, MONEY_DIGIT_SIZE);
+        break;
+    case '2':
+        break;
+    }
+    if (send(sock, msg, strlen(msg), 0) != strlen(msg))
+        DieWithError("send() sent a message of unexpected bytes");
+    // 受信処理
+    read_until_delim(sock, msg, '_', BUF_SIZE);
+    // 表示処理
+    printf("残高は%d円になりました。", atoi(msg));
+}
+void read_until_delim(int sock, char *buf, char delimiter, int max_length)
+{
+    int len_r = 0;
+    int index_letter = 0;
+
+    while (index_letter < max_length - 1)
+    {
+        //　一文字だけ受信
+
+        if ((len_r = recv(sock, buf + index_letter, 1, 0)) <= 0)
+        {
+            // エラー処理
+            printf("接続が切れました\n");
+            return;
+        }
+
+        if (buf[index_letter] == delimiter)
+            //区切り文字を受信→ループを抜ける。
+            break;
+        else
+            index_letter++;
+    }
+
+    buf[index_letter] = '\0';
 }
--- 3-Message/server.c
+++ 3-Message/server.c
@@ -3,16 +3,17 @@
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <stdlib.h>
-#include <unistd/h>
+#include <unistd.h>
 
 #define BUF_SIZE 256
 #define MONEY_DIGIT_SIZE 10
-
+int get_current_balance();
+void set_current_balance(int);
 void DieWithError(char *);
 int prepare_server_socket(int);
 void commun(int);
-
-int main(int argc, char *argc[])
+void read_until_delim(int, char *, char, int);
+int main(int argc, char *argv[])
 {
     struct sockaddr_in clientAddress;
     unsigned int szClientAddr;
@@ -25,7 +26,7 @@ int main(int argc, char *argc[])
     while (1)
     {
         szClientAddr = sizeof(clientAddress);
-        clisock = accept(servSock, (struct sockaddr *)&clientAddress, &szClientAddr);
+        cliSock = accept(servSock, (struct sockaddr *)&clientAddress, &szClientAddr);
 
         commun(cliSock);
 
@@ -48,10 +49,61 @@ int prepare_server_socket(int port)
         DieWithError("socket() failed");
 
     struct sockaddr_in servAddress;
-    servAddress.sin.family = AF_INET;
-    servAddress.sin.addr.s_addr = htonl(INADDR_ANY);
+    servAddress.sin_family = AF_INET;
+    servAddress.sin_addr.s_addr = htonl(INADDR_ANY);
     servAddress.sin_port = htons(port);
 
     bind(servSock, (struct sockaddr *)&servAddress, sizeof(servAddress));
     return servSock;
+}
+void read_until_delim(int sock, char *buf, char delimiter, int max_length)
+{
+    char msg[BUF_SIZE];
+    int len_r = 0;
+    int index_letter = 0;
+
+    while (index_letter < max_length - 1)
+    {
+        //　一文字だけ受信
+
+        if ((len_r = recv(sock, buf + index_letter, 1, 0)) <= 0)
+        {
+            // エラー処理
+            printf("接続が切れました\n");
+            return;
+        }
+
+        if (buf[index_letter] == delimiter)
+            //区切り文字を受信→ループを抜ける。
+            break;
+        else
+            index_letter++;
+    }
+
+    buf[index_letter] = '\0';
+}
+int get_current_balance()
+{
+    return 1000000;
+}
+
+void set_current_balance(int new_balance)
+{
+    return;
+}
+void commun(int sock)
+{
+    char buf[BUF_SIZE];
+    int balance = get_current_balance();
+
+    read_until_delim(sock, buf, '_', BUF_SIZE);
+    balance += atoi(buf); //預け入れ金額をプラス
+    read_until_delim(sock, buf, '_', BUF_SIZE);
+    balance -= atoi(buf);
+
+    set_current_balance(balance);
+
+    sprintf(buf, "%d_", balance);
+    if (send(sock, buf, strlen(buf), 0) != strlen(buf))
+        DieWithError("send() sent a message of unexpected bytes");
 }
